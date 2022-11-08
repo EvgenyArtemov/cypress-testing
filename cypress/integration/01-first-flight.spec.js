41bdfc6ce39e19cdd1f1b2ecb5af89686d8129be
Evgeny Artemov <eartemov013@gmail.com>
2022-11-08 18:48:19 +0400

    first setup of cypress

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter')
+  })
+
+  it('should have a form', () => {
+    cy.get('form').should('exist')
+  })
+
+  it('should have "Add Item Button"', () => {
+    cy.contains('Add Item');
+  });
+
+  it('should be able to enter a value in input', () => {
+    cy.get('[data-test="new-item-input"]').type('Good attitude')
+  })
+});
