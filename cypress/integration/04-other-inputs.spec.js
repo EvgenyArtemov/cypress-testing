ad36b03cca03da3871ca93509ea078e06fbaa3cc
DIEZ <eartemov013@gmail.com>
2022-11-14 00:15:44 +0400

    other input exercise

--- cypress/integration/04-other-inputs.spec.js
+++ cypress/integration/04-other-inputs.spec.js
@@ -7,10 +7,19 @@ describe('Secret Menu Items', () => {
     cy.get('#minimum-rating-visibility').as('rating-filter');
     cy.get('#restaurant-visibility-filter').as('restaurant-filter');
   });
+  it('should set the range and verify it', () => {
+    cy.get('@rating-filter').invoke('val', '7').trigger('input');
+    cy.get('@rating-filter').should('have.value', '7');
+  });
 
-  it('should set the range and verify it', () => {});
+  it('should check the checkbox and verify it', () => {
+    cy.get('input[type="checkbox"]').as('checkbox').check();
+    cy.get('@checkbox').should('be.checked');
+  });
 
-  it('should check the checkbox and verify it', () => {});
-
-  it('should select an option from the select and verify it', () => {});
+  it('should select an option from the select and verify it', () => {
+    const choice = 'Taco Bell';
+    cy.get('@restaurant-filter').select(choice);
+    cy.get('@restaurant-filter').should('have.value', choice);
+  });
 });
